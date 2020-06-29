4cde95afc46b0b2143bc51bfa0f049c80885d31d
Thiago <thiagonunesal1307@gmail.com>
2020-06-29 16:24:43 -0300

    concluído

---
+++ Produto.cs
@@ -0,0 +1,27 @@
+namespace AulaPOOCsv
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        private const string Path = "Database/produto.csv";
+
+        public Produto () {
+            if (!File.Exists(Path)) {
+                File.Create(Path).Close();
+            }
+        }
+        private string PrepararLinhaCSV (Produto p) {
+            return $"{p.Codigo};{p.Nome};{p.Preco}";
+        }
+        public void Inserir (Produto p) {
+            string[] linha = new string[] {PrepararLinhaCSV(p)};
+            File.AppendAllLines(Path, linha);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AulaPOOCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Gibson SG";
+            p1.Preco = 4700f;
+
+            p1.Inserir(p1);
+        }
+    }
+}
