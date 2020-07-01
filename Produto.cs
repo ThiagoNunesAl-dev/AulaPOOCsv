fec923663b973fdd8f655324c425485cd6c4f46a
Thiago <thiagonunesal1307@gmail.com>
2020-07-01 16:42:24 -0300

    leitura concluída

--- Produto.cs
+++ Produto.cs
@@ -3,6 +3,7 @@ namespace AulaPOOCsv
     using System;
     using System.IO;
     using System.Collections.Generic;
+    using System.Linq;
     public class Produto
     {
         public int Codigo { get; set; }
@@ -11,17 +12,61 @@ namespace AulaPOOCsv
 
         private const string Path = "Database/Produto.csv";
 
+        //Cria uma pasta Produto.csv (conforme o caminho criado acima), se esta não existir.
         public Produto () {
             if (!File.Exists(Path)) {
                 File.Create(Path).Close();
             }
         }
+
+        //Prepara uma linha no formato padrão csv.
         private string PrepararLinhaCSV (Produto p) {
-            return $"{p.Codigo};{p.Nome};{p.Preco}";
+            return $"Código:{p.Codigo};Nome:{p.Nome};Preço:{p.Preco}";
         }
+
+        //Insere a linha criada e preenchida conforme o método acima no documento csv.
+        //O formato suportado é de lista, então é necessário transformar a string preparada anteriormente em uma lista.
         public void Inserir (Produto p) {
             string[] linha = new string[] {PrepararLinhaCSV(p)};
+            //Append acrescenta as linhas preparadas ao documento csv.
             File.AppendAllLines(Path, linha);
+        }
+
+        //Separa os dados.
+        public string Separar (string dado) {
+            //Ex.:
+            //dado => Código:1
+            //dado[0] => Código
+            //dado[1] => 1
+            //Retorna o número do código, sem o enunciado.
+            return dado.Split(":")[1];
+        }
+
+        public List<Produto> Ler () {
+            //Cria-se uma lista de produtos padrão.
+            List<Produto> produtos = new List<Produto>();
+            //Transforma-se as linhas criadas em um arranjo de strings.
+            string[] linhas = File.ReadAllLines(Path);
+            //O array é varrido.
+
+            foreach (var linha in linhas)
+            {
+                //Os dados das linhas são separados.
+                string[] dados = linha.Split(";");
+                
+                //Os dados agora são tratados para que possam ser colocados na lista.
+                Produto prod = new Produto();
+                prod.Codigo = Int32.Parse(Separar(dados[0]));
+                prod.Nome = Separar(dados[1]);
+                prod.Preco = float.Parse(Separar(dados[2]));
+
+                //Os dados são inseridos na lista.
+                produtos.Add(prod);
+            }
+
+            produtos = produtos.OrderBy(z => z.Nome).ToList();
+            
+            return produtos;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,8 +12,21 @@ namespace AulaPOOCsv
             p1.Codigo = 1;
             p1.Nome = "Gibson SG";
             p1.Preco = 4700f;
-
             p1.Inserir(p1);
+
+            Produto p2 = new Produto();
+            p2.Codigo = 2;
+            p2.Nome = "Ibanez RG440V";
+            p2.Preco = 5000f;
+            p2.Inserir(p2);
+
+            List<Produto> lista = new List<Produto>();
+            lista = p1.Ler();
+
+            foreach (Produto item in lista)
+            {
+                Console.WriteLine($"{item.Nome} - R$ {item.Preco}");
+            }
         }
     }
 }
